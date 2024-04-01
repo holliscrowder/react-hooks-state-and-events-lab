670300607b6c51abee8078549f9823ab506f509c
Hollis Crowder <hollis.crowder@gmail.com>
2024-04-01 15:05:14 -0700

    complete state and events lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [cartStatus, setCartStatus] = useState(true);
+  function handleCart() {
+    setCartStatus((cartStatus) => !cartStatus);
+  }
+  const liClass = cartStatus ? "" : "in-cart";
+
   return (
-    <li className="">
+    <li className={liClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleCart}>
+        {cartStatus ? "Add to Cart" : "Remove From Cart"}
+      </button>
     </li>
   );
 }
